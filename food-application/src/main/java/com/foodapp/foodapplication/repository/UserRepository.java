4aaae542cec193b5d39763a28dbc5a589ec3856e
ratnasai2206 <kothamasuvenkataratnasai@gmail.com>
2024-02-28 16:51:26 +0530

    util packages added

--- food-application/src/main/java/com/foodapp/foodapplication/repository/UserRepository.java
+++ food-application/src/main/java/com/foodapp/foodapplication/repository/UserRepository.java
@@ -1,9 +1,13 @@
 package com.foodapp.foodapplication.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.foodapp.foodapplication.entity.Users;
 
 public interface UserRepository extends JpaRepository<Users, Integer> {
 
+	@Query("SELECT u FROM User u WHERE u.phoneNumber = :phoneNumber")
+	Users findByPhoneNumber(@Param("phoneNumber") long phoneNumber);
 }
---
+++ food-application/src/main/java/com/foodapp/foodapplication/util/UserRoles.java
@@ -0,0 +1,8 @@
+package com.foodapp.foodapplication.util;
+
+public enum UserRoles {
+
+	BRANCHMANAGER,
+	STAFF,
+	CUSTOMER
+}
