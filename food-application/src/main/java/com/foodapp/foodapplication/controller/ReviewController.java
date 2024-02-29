6ffd19835f832b8b99a35e4f593596df36765565
Anugrahas2001 <anugrahas2001@gmail.com>
2024-02-29 18:25:08 +0530

    Review added and review deleted

--- food-application/src/main/java/com/foodapp/foodapplication/controller/ReviewController.java
+++ food-application/src/main/java/com/foodapp/foodapplication/controller/ReviewController.java
@@ -3,15 +3,16 @@ package com.foodapp.foodapplication.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.foodapp.foodapplication.dto.ResponseStructure;
+import com.foodapp.foodapplication.dto.ReviewDto;
 import com.foodapp.foodapplication.entity.Review;
 import com.foodapp.foodapplication.services.ReviewService;
 
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 @RestController
 public class ReviewController {
@@ -21,9 +22,9 @@ public class ReviewController {
 	
 	
 	//save Review
-	@GetMapping("/foodapp/Reviews/{userId}/{orderId}")
+	@PostMapping("/foodapp/Reviews/{userId}/{orderId}")
 	public ResponseEntity<ResponseStructure<Review>> saveReview(@PathVariable int userId, @PathVariable
-			int orderId,@RequestBody Review review)
+			int orderId,@RequestBody ReviewDto review)
 	{
 		return reviewservice.saveReview(userId,orderId,review);
 	}
