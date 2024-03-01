382742fff910c64d823d7b1abfe896e917c4e560
Nakul240 <nakulnanjappa240@gmail.com>
2024-03-01 09:48:37 +0530

    before pull

--- food-application/src/main/java/com/foodapp/foodapplication/excpection/ApplicationExceptionHandler.java
+++ food-application/src/main/java/com/foodapp/foodapplication/excpection/ApplicationExceptionHandler.java
@@ -8,59 +8,64 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 
 import com.foodapp.foodapplication.dto.ResponseStructure;
 
-
 @ControllerAdvice
 public class ApplicationExceptionHandler extends ResponseEntityExceptionHandler {
-	
-	
+
 	@ExceptionHandler(UsersAlreadyExistException.class)
-	public ResponseEntity<ResponseStructure<String>> catchUserAlreadyExitException(UsersAlreadyExistException doesNotPresentException){
-		ResponseStructure<String> responseStructure=new ResponseStructure<String>();
+	public ResponseEntity<ResponseStructure<String>> catchUserAlreadyExitException(
+			UsersAlreadyExistException doesNotPresentException) {
+		ResponseStructure<String> responseStructure = new ResponseStructure<String>();
 		responseStructure.setMessage("User Already Exists in the data base");
 		responseStructure.setStatusCode(HttpStatus.FOUND.value());
 		responseStructure.setData(doesNotPresentException.getMessage());
-		return new ResponseEntity<ResponseStructure<String>>(responseStructure,HttpStatus.FOUND);
+		return new ResponseEntity<ResponseStructure<String>>(responseStructure, HttpStatus.FOUND);
 	}
-	
-
 
 	@ExceptionHandler(UsersNotExistException.class)
-	public ResponseEntity<ResponseStructure<String>> catchUserNotExitException(UsersNotExistException doesNotPresentException){
-		ResponseStructure<String> responseStructure=new ResponseStructure<String>();
+	public ResponseEntity<ResponseStructure<String>> catchUserNotExitException(
+			UsersNotExistException doesNotPresentException) {
+		ResponseStructure<String> responseStructure = new ResponseStructure<String>();
 		responseStructure.setMessage("User Not Exists in the data base");
 		responseStructure.setStatusCode(HttpStatus.NOT_FOUND.value());
 		responseStructure.setData(doesNotPresentException.getMessage());
-		return new ResponseEntity<ResponseStructure<String>>(responseStructure,HttpStatus.NOT_FOUND);
+		return new ResponseEntity<ResponseStructure<String>>(responseStructure, HttpStatus.NOT_FOUND);
 	}
-	
+
 	@ExceptionHandler(EmptyOderException.class)
-	public ResponseEntity<ResponseStructure<String>> catchEmptyOrderException(EmptyOderException emptyexception){
-		ResponseStructure<String> responseStructure=new ResponseStructure<String>();
+	public ResponseEntity<ResponseStructure<String>> catchEmptyOrderException(EmptyOderException emptyexception) {
+		ResponseStructure<String> responseStructure = new ResponseStructure<String>();
 		responseStructure.setMessage("User Order is empty");
 		responseStructure.setStatusCode(HttpStatus.NOT_FOUND.value());
 		responseStructure.setData(emptyexception.getMessage());
-		return new ResponseEntity<ResponseStructure<String>>(responseStructure,HttpStatus.NOT_FOUND);
+		return new ResponseEntity<ResponseStructure<String>>(responseStructure, HttpStatus.NOT_FOUND);
 	}
-
 
 	@ExceptionHandler(ItemNotFoundException.class)
-	public ResponseEntity<ResponseStructure<String>> catchItemNotFoundException(ItemNotFoundException notFoundException){
-		ResponseStructure<String> responseStructure=new ResponseStructure<String>();
+	public ResponseEntity<ResponseStructure<String>> catchItemNotFoundException(
+			ItemNotFoundException notFoundException) {
+		ResponseStructure<String> responseStructure = new ResponseStructure<String>();
 		responseStructure.setMessage("Item Not found");
-		responseStructure.setStatusCode(HttpStatus.FOUND.value());
+		responseStructure.setStatusCode(HttpStatus.NOT_FOUND.value());
 		responseStructure.setData(notFoundException.getMessage());
-		return new ResponseEntity<ResponseStructure<String>>(responseStructure,HttpStatus.FOUND);
+		return new ResponseEntity<ResponseStructure<String>>(responseStructure, HttpStatus.NOT_FOUND);
+	}
+
+	@ExceptionHandler(IdNotFoundException.class)
+	public ResponseEntity<ResponseStructure<String>> catchIdNotFound(IdNotFoundException idnotfoundexception) {
+		ResponseStructure<String> responseStructure = new ResponseStructure<String>();
+		responseStructure.setStatusCode(HttpStatus.NOT_FOUND.value());
+		responseStructure.setMessage("Manager Id is not present");
+		responseStructure.setData(idnotfoundexception.getMessage());
+		return new ResponseEntity<ResponseStructure<String>>(responseStructure, HttpStatus.NOT_FOUND);
 	}
 	
-	
-      @ExceptionHandler(IdNotFoundException.class)
-     public ResponseEntity<ResponseStructure<String>> catchIdNotFound(IdNotFoundException idnotfoundexception)
-  {
-	ResponseStructure<String> responseStructure=new ResponseStructure<String>();
-	responseStructure.setStatusCode(HttpStatus.NOT_FOUND.value());
-	responseStructure.setMessage("Manager Id is not present");
-	responseStructure.setData(idnotfoundexception.getMessage());
-	return new ResponseEntity<ResponseStructure<String>>(responseStructure,HttpStatus.NOT_FOUND);
-  }
+	@ExceptionHandler(OrderedQuantityNotAvailable.class)
+	public ResponseEntity<ResponseStructure<String>> catchOrderedQuantityNotAvailable(OrderedQuantityNotAvailable exception) {
+		ResponseStructure<String> responseStructure = new ResponseStructure<String>();
+		responseStructure.setStatusCode(HttpStatus.BAD_REQUEST.value());
+		responseStructure.setMessage("Item quantity does not match the requirement");
+		responseStructure.setData(exception.getMessage());
+		return new ResponseEntity<ResponseStructure<String>>(responseStructure, HttpStatus.BAD_REQUEST);
+	}
 
 }
